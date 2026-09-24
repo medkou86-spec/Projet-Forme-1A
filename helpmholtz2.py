78c1e14a46dc9851598c0ddf935ac0995cee6254
mohamed kouira <med.kou86@gmail.com>
2026-09-24 11:10:29 +0200

    passage au support cplx

--- helpmholtz2.py
+++ helpmholtz2.py
@@ -5,45 +5,50 @@ from petsc4py import PETSc
 from dolfinx.fem.petsc import LinearProblem
 import ufl
 
-k=20
-nx,ny=80,80
-#maillage du carré oméga
-domain=mesh.create_unit_square(MPI.COMM_WORLD,nx,ny)
+k = 20-2j
+nx, ny = 80, 80
 
-#espace d'eléments finis P1
-V=fem.functionspace(domain,("Lagrange",1))
+# Maillage du carré oméga
+domain = mesh.create_unit_square(MPI.COMM_WORLD, nx, ny)
 
-#définition des bords
-fdim=domain.topology.dim-1
-def left(x): return np.isclose(x[0],0)
-def right(x): return np.isclose(x[0],1)
-def bottom(x): return np.isclose(x[1],0)
-def top(x): return np.isclose(x[1],1)   
-left_facets=mesh.locate_entities_boundary(domain,fdim,left)
-right_facets=mesh.locate_entities_boundary(domain,fdim,right)
-bottom_facets=mesh.locate_entities_boundary(domain,fdim,bottom)
-top_facets=mesh.locate_entities_boundary(domain,fdim,top)
+# Espace d'eléments finis P1
+V = fem.functionspace(domain, ("Lagrange", 1))
 
-#marquage des facettes(on n' besoin que du bord droit pour le terme de robin)
-right_marker=1
-marked_facets=np.hstack([right_facets])
-marked_values=np.hstack([np.full(len(right_facets),right_marker,dtype=np.int32)])
-sorted_facets=np.argsort(marked_facets)
-facet_tag=mesh.meshtags(domain,fdim,marked_facets[sorted_facets],marked_values[sorted_facets])
-ds=ufl.Measure("ds",domain=domain,subdomain_data=facet_tag)
+# Définition des bords
+fdim = domain.topology.dim - 1
+def left(x): return np.isclose(x[0], 0)
+def right(x): return np.isclose(x[0], 1)
+def bottom(x): return np.isclose(x[1], 0)
+def top(x): return np.isclose(x[1], 1)
 
-#condition de dirichlet u=1 sur le bord gauche
-uD=fem.Function(V)
-uD.interpolate(lambda x: np.full(x.shape[1],PETSc.ScalarType(1)))
-left_dofs=fem.locate_dofs_topological(V,fdim,left_facets)
-bc=fem.dirichletbc(uD,left_dofs)
+left_facets = mesh.locate_entities_boundary(domain, fdim, left)
+right_facets = mesh.locate_entities_boundary(domain, fdim, right)
 
-#formulation variationnelle en ufl
-u=ufl.TrialFunction(V)
-v=ufl.TestFunction(V)
-a=(ufl.inner(ufl.grad(u),ufl.grad(v))*ufl.dx-(k**2)*ufl.inner(u,v)*ufl.dx+1j*k*ufl.inner(u,v)*ds(right_marker))
-L=0
-#résolution du problème linéaire
+# Marquage des facettes (on a besoin que du bord droit pour le terme de robin)
+right_marker = 1
+marked_facets = np.hstack([right_facets])
+marked_values = np.hstack([np.full(len(right_facets), right_marker, dtype=np.int32)])
+sorted_facets = np.argsort(marked_facets)
+facet_tag = mesh.meshtags(domain, fdim, marked_facets[sorted_facets], marked_values[sorted_facets])
+ds = ufl.Measure("ds", domain=domain, subdomain_data=facet_tag)
+
+# Condition de dirichlet u=1 sur le bord gauche
+uD = fem.Function(V)
+uD.interpolate(lambda x: np.full(x.shape[1], PETSc.ScalarType(1)))
+left_dofs = fem.locate_dofs_topological(V, fdim, left_facets)
+bc = fem.dirichletbc(uD, left_dofs)
+
+# Formulation variationnelle en ufl
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
+
+a = (ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx 
+     - (k**2) * ufl.inner(u, v) * ufl.dx 
+     + 1j * k * ufl.inner(u, v) * ds(right_marker))
+
+# CORRECTION : L doit être une forme linéaire valide sur l'espace test v
+L = fem.Constant(domain, PETSc.ScalarType(0.0)) * ufl.conj(v) * ufl.dx
+# Résolution du problème linéaire
 problem = LinearProblem(
     a, L,
     bcs=[bc],
@@ -53,5 +58,6 @@ problem = LinearProblem(
     },
     petsc_options_prefix="helmholtz"
 )
-uh=problem.solve()
+uh = problem.solve()
 
+print("Résolution terminée avec succès ! Le support complexe est actif.")
